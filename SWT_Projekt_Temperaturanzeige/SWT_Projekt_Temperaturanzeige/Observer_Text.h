e3062155bdf58a197d9aef41a3d9d55cfc62aa51
JohannWeisenburger <johann.weisenburger@iem.thm.de>
2014-07-18 22:37:30 +0200

    GTK includes, libs
    Includes und libs für GTK eingebunden

--- SWT_Projekt_Temperaturanzeige/SWT_Projekt_Temperaturanzeige/Observer_Text.h
+++ SWT_Projekt_Temperaturanzeige/SWT_Projekt_Temperaturanzeige/Observer_Text.h
@@ -1,6 +1,11 @@
+#ifndef _Observer_Text_H__
+#define _Observer_Text_H__
+
 #include "Observer_Graph.h"
 
 class Observer_Text : public Observer_Graph {
 public:
 	void update(string t_orte[], double t_val[], string t_metadaten[]);
 };
+
+#endif
