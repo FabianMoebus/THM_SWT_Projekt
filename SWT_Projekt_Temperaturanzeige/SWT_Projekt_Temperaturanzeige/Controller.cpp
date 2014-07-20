b7caa3ede0d7b4080b7a91014047e2e7d2d758f8
FabianMoebus <fabian.moebus@iem.thm.de>
2014-07-20 12:55:36 +0200

    GTK imported

--- SWT_Projekt_Temperaturanzeige/SWT_Projekt_Temperaturanzeige/Controller.cpp
+++ SWT_Projekt_Temperaturanzeige/SWT_Projekt_Temperaturanzeige/Controller.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 void Controller::run(string url) {
-	/*
-	 * while (button(Beenden) nicht gedrückt) {
-			  subject.do_update(url);
-	   }	
-	*/
+	
+	 subject.do_update(url);
+	 cout << subject.t_metadaten[0] << endl;
+	 cout << subject.t_metadaten[1] << endl;
+	 for (int i = 0; i < 6; i++) {
+		 cout << subject.t_orte[i] << ":  " << subject.t_val[i] << endl;
+	 }
 	_sleep(60000);
 }
